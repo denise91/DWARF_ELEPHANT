97e865ee9857bc34806f49e5babdec7264e4afd5
nikhilgv91 <nikhilvaidya91@gmail.com>
2018-10-31 11:46:53 +0100

    Simple 2D geometrical parameterization works. split_matrix_according_to_subdomain should be false for manual matrix-subdomain assigment to work.

--- include/userobjects/DwarfElephantGeom2DRBThetaExpansion.h
+++ include/userobjects/DwarfElephantGeom2DRBThetaExpansion.h
@@ -34,7 +34,8 @@ struct Geom2DRBThetaExpansion : RBThetaExpansion
 {
   Geom2DRBThetaExpansion()
   {
-  attach_A_theta(&_rb_theta);
+  attach_A_theta(&_rb_theta);//&Geom2DRB_ATheta_HeatSource_Outer_0);
+  
   attach_A_theta(&Geom2DRB_ATheta_DiffusionXX_maxZ1_0);
   attach_A_theta(&Geom2DRB_ATheta_DiffusionXY_maxZ1_0);
   attach_A_theta(&Geom2DRB_ATheta_DiffusionYX_maxZ1_0);
@@ -43,44 +44,10 @@ struct Geom2DRBThetaExpansion : RBThetaExpansion
   attach_A_theta(&Geom2DRB_ATheta_DiffusionXY_maxZ2_0);
   attach_A_theta(&Geom2DRB_ATheta_DiffusionYX_maxZ2_0);
   attach_A_theta(&Geom2DRB_ATheta_DiffusionYY_maxZ2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXX_maxX1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXY_maxX1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYX_maxX1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYY_maxX1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXX_maxX2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXY_maxX2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYX_maxX2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYY_maxX2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXX_minZ1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXY_minZ1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYX_minZ1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYY_minZ1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXX_minZ2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXY_minZ2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYX_minZ2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYY_minZ2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXX_minX1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXY_minX1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYX_minX1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYY_minX1_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXX_minX2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXY_minX2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYX_minX2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYY_minX2_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionXX_BoundingBox_0);
-  attach_A_theta(&Geom2DRB_ATheta_DiffusionYY_BoundingBox_0);
-  attach_A_theta(&Geom2DRB_ATheta_Perfusion_BoundingBox_0);
-  attach_F_theta(&Geom2DRB_FTheta_HeatSource_maxZ1_0);
+  
   attach_F_theta(&Geom2DRB_FTheta_HeatSource_maxZ2_0);
-  attach_F_theta(&Geom2DRB_FTheta_HeatSource_maxX1_0);
-  attach_F_theta(&Geom2DRB_FTheta_HeatSource_maxX2_0);
-  attach_F_theta(&Geom2DRB_FTheta_HeatSource_minZ1_0);
-  attach_F_theta(&Geom2DRB_FTheta_HeatSource_minZ2_0);
-  attach_F_theta(&Geom2DRB_FTheta_HeatSource_minX1_0);
-  attach_F_theta(&Geom2DRB_FTheta_HeatSource_minX2_0);
-  attach_F_theta(&Geom2DRB_FTheta_HeatSource_BoundingBox_0);
+
   }
-  RBTheta _rb_theta;
   Geom2DRB_ATheta_DiffusionXX_maxZ1 Geom2DRB_ATheta_DiffusionXX_maxZ1_0;
   Geom2DRB_ATheta_DiffusionXY_maxZ1 Geom2DRB_ATheta_DiffusionXY_maxZ1_0;
   Geom2DRB_ATheta_DiffusionYX_maxZ1 Geom2DRB_ATheta_DiffusionYX_maxZ1_0;
@@ -89,42 +56,11 @@ struct Geom2DRBThetaExpansion : RBThetaExpansion
   Geom2DRB_ATheta_DiffusionXY_maxZ2 Geom2DRB_ATheta_DiffusionXY_maxZ2_0;
   Geom2DRB_ATheta_DiffusionYX_maxZ2 Geom2DRB_ATheta_DiffusionYX_maxZ2_0;
   Geom2DRB_ATheta_DiffusionYY_maxZ2 Geom2DRB_ATheta_DiffusionYY_maxZ2_0;
-  Geom2DRB_ATheta_DiffusionXX_maxX1 Geom2DRB_ATheta_DiffusionXX_maxX1_0;
-  Geom2DRB_ATheta_DiffusionXY_maxX1 Geom2DRB_ATheta_DiffusionXY_maxX1_0;
-  Geom2DRB_ATheta_DiffusionYX_maxX1 Geom2DRB_ATheta_DiffusionYX_maxX1_0;
-  Geom2DRB_ATheta_DiffusionYY_maxX1 Geom2DRB_ATheta_DiffusionYY_maxX1_0;
-  Geom2DRB_ATheta_DiffusionXX_maxX2 Geom2DRB_ATheta_DiffusionXX_maxX2_0;
-  Geom2DRB_ATheta_DiffusionXY_maxX2 Geom2DRB_ATheta_DiffusionXY_maxX2_0;
-  Geom2DRB_ATheta_DiffusionYX_maxX2 Geom2DRB_ATheta_DiffusionYX_maxX2_0;
-  Geom2DRB_ATheta_DiffusionYY_maxX2 Geom2DRB_ATheta_DiffusionYY_maxX2_0;
-  Geom2DRB_ATheta_DiffusionXX_minZ1 Geom2DRB_ATheta_DiffusionXX_minZ1_0;
-  Geom2DRB_ATheta_DiffusionXY_minZ1 Geom2DRB_ATheta_DiffusionXY_minZ1_0;
-  Geom2DRB_ATheta_DiffusionYX_minZ1 Geom2DRB_ATheta_DiffusionYX_minZ1_0;
-  Geom2DRB_ATheta_DiffusionYY_minZ1 Geom2DRB_ATheta_DiffusionYY_minZ1_0;
-  Geom2DRB_ATheta_DiffusionXX_minZ2 Geom2DRB_ATheta_DiffusionXX_minZ2_0;
-  Geom2DRB_ATheta_DiffusionXY_minZ2 Geom2DRB_ATheta_DiffusionXY_minZ2_0;
-  Geom2DRB_ATheta_DiffusionYX_minZ2 Geom2DRB_ATheta_DiffusionYX_minZ2_0;
-  Geom2DRB_ATheta_DiffusionYY_minZ2 Geom2DRB_ATheta_DiffusionYY_minZ2_0;
-  Geom2DRB_ATheta_DiffusionXX_minX1 Geom2DRB_ATheta_DiffusionXX_minX1_0;
-  Geom2DRB_ATheta_DiffusionXY_minX1 Geom2DRB_ATheta_DiffusionXY_minX1_0;
-  Geom2DRB_ATheta_DiffusionYX_minX1 Geom2DRB_ATheta_DiffusionYX_minX1_0;
-  Geom2DRB_ATheta_DiffusionYY_minX1 Geom2DRB_ATheta_DiffusionYY_minX1_0;
-  Geom2DRB_ATheta_DiffusionXX_minX2 Geom2DRB_ATheta_DiffusionXX_minX2_0;
-  Geom2DRB_ATheta_DiffusionXY_minX2 Geom2DRB_ATheta_DiffusionXY_minX2_0;
-  Geom2DRB_ATheta_DiffusionYX_minX2 Geom2DRB_ATheta_DiffusionYX_minX2_0;
-  Geom2DRB_ATheta_DiffusionYY_minX2 Geom2DRB_ATheta_DiffusionYY_minX2_0;
-  Geom2DRB_ATheta_DiffusionXX_BoundingBox Geom2DRB_ATheta_DiffusionXX_BoundingBox_0;
-  Geom2DRB_ATheta_DiffusionYY_BoundingBox Geom2DRB_ATheta_DiffusionYY_BoundingBox_0;
-  Geom2DRB_ATheta_Perfusion_BoundingBox Geom2DRB_ATheta_Perfusion_BoundingBox_0;
-  Geom2DRB_FTheta_HeatSource_maxZ1 Geom2DRB_FTheta_HeatSource_maxZ1_0;
+
+
   Geom2DRB_FTheta_HeatSource_maxZ2 Geom2DRB_FTheta_HeatSource_maxZ2_0;
-  Geom2DRB_FTheta_HeatSource_maxX1 Geom2DRB_FTheta_HeatSource_maxX1_0;
-  Geom2DRB_FTheta_HeatSource_maxX2 Geom2DRB_FTheta_HeatSource_maxX2_0;
-  Geom2DRB_FTheta_HeatSource_minZ1 Geom2DRB_FTheta_HeatSource_minZ1_0;
-  Geom2DRB_FTheta_HeatSource_minZ2 Geom2DRB_FTheta_HeatSource_minZ2_0;
-  Geom2DRB_FTheta_HeatSource_minX1 Geom2DRB_FTheta_HeatSource_minX1_0;
-  Geom2DRB_FTheta_HeatSource_minX2 Geom2DRB_FTheta_HeatSource_minX2_0;
-  Geom2DRB_FTheta_HeatSource_BoundingBox Geom2DRB_FTheta_HeatSource_BoundingBox_0;
+
+  RBTheta _rb_theta;
 };
 
 //------------------------------
